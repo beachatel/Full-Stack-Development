3748a9636745ab08556ffa57bed23d212b35d243
bea <bchatelbell@gmail.com>
2026-03-20 16:29:45 +0000

    Testing for final project

---
+++ assessment/tests/bike-usability/src/index.js
@@ -0,0 +1,16 @@
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+
+// // Enable CORS for all origins - NOT for production!
+// app.use(cors());
+
+// app.get(
+//   "https://github.com/MaxHalford/bike-sharing-history/blob/main/data/stations/manchester/beryl.geojson/",
+//   (req, res) => {
+//     res.json({ message: "This is accessible from any origin" });
+//   },
+// );
+
+// app.listen(3000);
---
+++ assessment/tests/bike-usability/src/main.js
@@ -0,0 +1,4 @@
+const api =
+  "https://github.com/MaxHalford/bike-sharing-history/blob/main/data/stations/manchester/beryl.geojson";
+
+fetch(api);
